db3532e635fd950fc8a83eadfbc918209e9c1d3a
Mohamed-Samiir <mohammedsamiir1996@gmail.com>
2022-01-08 17:22:52 +0200

    first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-ng-app';
+  textContent: string | undefined;
+  numberContent: number = 0;
+  isEven: boolean = true;
+  minMax: number | undefined
+  numbers: Array<number> = [1,2,3,4,5]
+  findifEven () {
+    this.isEven = this.numberContent % 2 == 0;
+  };
+  findMax(){
+    this.minMax = Math.max.apply(null, this.numbers)
+  }
+  findMin(){
+    this.minMax = Math.min.apply(null, this.numbers)
+  }
 }
